cc6d535a8c8afdd5ea73af084e447f4f8d60d480
A Name <yeetoky@gmail.com>
2025-04-24 13:22:38 -0500

    Syncing up !!!

--- Assets/ColorPicker.cs
+++ Assets/ColorPicker.cs
@@ -1,37 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class ColorPicker : MonoBehaviour
 {
-    public string CurrentColor = "hell";
+    public string CurrentColor = "Green";
+    public Button colorButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        tag = "Triangle/Green";
-        
+        CurrentColor = "Green";
+        Button btn = colorButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    {
+        if (CurrentColor == "Green")
+        {
+            CurrentColor = "Blue";
+        }
+        else if(CurrentColor == "Blue")
+        {
+            CurrentColor = "Yellow";
+        }
+        else if(CurrentColor == "Yellow")
+        {
+            CurrentColor = "Gray";
+        }
+        else if(CurrentColor == "Gray")
+        {
+            CurrentColor = "Green";
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    void changeColor()
-    {
-        if(tag == "Triangle/Green")
-        {
-            tag = "Crescent/Blue";
-        }
-        else if (tag == "Crescent/Blue")
-        {
-            tag = "Star/Yellow";
-        }
-        else if (tag == "Star/Yellow")
-        {
-            tag = "YinYang";
-        }
-        else if (tag == "YingYang")
-        {
-            tag = "Triangle/Green";
-        }
     }
 }
--- Assets/CrescentPathScript.cs
+++ Assets/CrescentPathScript.cs
@@ -1,13 +1,28 @@
  using UnityEngine;
+ using UnityEngine.UI;
+ using System.Collections;
+ using UnityEditor;
 
 public class CrescentPathScript : MonoBehaviour
 {
-    public string correctColor, currentColor;
+    public string correctColor;
+    public string currentColor;
+    public MonoScript balalala;
+    public Button crescButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
         correctColor = "Blue";
         currentColor = "None";
+        Button btn = crescButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        currentColor = GameObject.Find("ColorChanger").GetComponent<ColorPicker>().CurrentColor;
     }
 
     // Update is called once per frame
